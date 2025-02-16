1aa9b92ed3e7fd49c4fed50ff146021a5ee9b637
Harsh Dembla <harshdembla80@gmail.com>
2025-02-16 02:06:19 -0800

    Update task2.cpp

--- lab03/task2.cpp
+++ lab03/task2.cpp
@@ -1,109 +1,97 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class FitnessTracker{
-string username;
-int dailystepsgoal;
-int stepstaken;
-float caloriesburned;
-float caloriesburnedgoal;
+
+class FitnessTracker {
+private:
+    string username;
+    int dailystepsgoal;
+    int stepstaken;
+    float caloriesburned;
+    float caloriesburnedgoal;
 
 public:
-FitnessTracker(string username){
-this->username=username;	
-	
-}	
+   
+    FitnessTracker(string username) {
+        this->username = username;
+        this->dailystepsgoal = 0;
+        this->stepstaken = 0;
+        this->caloriesburned = 0.0;
+        this->caloriesburnedgoal = 0.0;
+    }
 
-void setsteps(int stepstaken,int dailystepsgoal){
-	this->stepstaken=stepstaken;
-	this->dailystepsgoal=dailystepsgoal;
-	
-}
+    
+    void setSteps(int stepstaken, int dailystepsgoal) {
+        this->stepstaken = stepstaken;
+        this->dailystepsgoal = dailystepsgoal;
+    }
 
-void calcluatecalories(){
+    void calculateCalories() {
+        float perStep = 0.5;
+        caloriesburned = perStep * stepstaken;
+        caloriesburnedgoal = dailystepsgoal * perStep;
+        cout << "Expected Calories Burned: " << caloriesburnedgoal << endl;
+        cout << "Actual Calories Burned: " << caloriesburned << endl;
+    }
 
-float perstep=0.5;
-caloriesburned=perstep*stepstaken;
-caloriesburnedgoal=dailystepsgoal*perstep;
-cout<<"Expected Calories burned: "<<caloriesburnedgoal<<endl;
-cout<<"Actual Calories Burned: "<<caloriesburned<<endl;	
+   
+    void reminder() {
+        float perStep = 0.5;
+        if (dailystepsgoal > stepstaken) {
+            int remain = dailystepsgoal - stepstaken;
+            float remainingCalories = remain * perStep;
+            cout << "You still need to burn " << remainingCalories << " calories to complete your daily goal." << endl;
+        } else {
+            cout << "You have achieved your goal!" << endl;
+        }
+    }
 
-
-
-
-}
-
-void remainder(){
-float perstep=0.5;	
-
-if(dailystepsgoal>stepstaken){
-int remain=dailystepsgoal-stepstaken;
-caloriesburned=remain*perstep;
-cout<<"You still need to burn "<<caloriesburned<<" calories For completing your daily goal: "<<endl;
-	
-}
-else{
-cout<<"you have achieved your goal: "<<endl;	
-}
-	
-}
-
-void display(){
-cout<<"Name: "<<username<<endl;
-cout<<"Daily Steps Goal: "<<dailystepsgoal<<endl;
-cout<<"Steps taken: "<<stepstaken<<endl;
-cout<<"Expected Calories burned: "<<caloriesburnedgoal<<endl;
-cout<<"Actual Calories Burned: "<<caloriesburned<<endl;	
-}
-
-
-
-	
+    
+    void display() {
+        cout << "Name: " << username << endl;
+        cout << "Daily Steps Goal: " << dailystepsgoal << endl;
+        cout << "Steps Taken: " << stepstaken << endl;
+        cout << "Expected Calories Burned: " << caloriesburnedgoal << endl;
+        cout << "Actual Calories Burned: " << caloriesburned << endl;
+    }
 };
-int main(){
-FitnessTracker f("Laiba");
 
 
-int choice;
-do{
-cout<<"1: LOG STEPS: "<<endl;
-cout<<"2: DISPLAY CALORIES: "<<endl;
-cout<<"3: DISPLAY PROGESS: "<<endl;
-cout<<"4: EXIT: "<<endl;
-cout<<"Enter your choice! "<<endl;
-cin>>choice;
+int main() {
+    FitnessTracker f("Laiba");
 
-switch(choice){
-case 1:
-int stepstaken,dailystepsgoal;
-cout<<"Enter the daily steps Goal: "<<endl;
-cin>>dailystepsgoal;
+    int choice;
+    do {
+        cout << "1: LOG STEPS" << endl;
+        cout << "2: DISPLAY CALORIES" << endl;
+        cout << "3: DISPLAY PROGRESS" << endl;
+        cout << "4: EXIT" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-cout<<"Enter the Actual steps you have taken :"<<endl;
-cin>>stepstaken;
+        switch (choice) {
+            case 1: {
+                int stepstaken, dailystepsgoal;
+                cout << "Enter the daily steps goal: ";
+                cin >> dailystepsgoal;
+                cout << "Enter the actual steps you have taken: ";
+                cin >> stepstaken;
+                f.setSteps(stepstaken, dailystepsgoal);
+                break;
+            }
+            case 2:
+                f.calculateCalories();
+                break;
+            case 3:
+                f.display();
+                f.reminder();
+                break;
+            case 4:
+                cout << "Exiting the program..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice! Please enter a valid option." << endl;
+        }
+    } while (choice != 4);
 
-f.setsteps(stepstaken,dailystepsgoal);
-break;
-
-case 2:
-f.calcluatecalories();	
-break;
-
-case 3:
-f.display();
-f.remainder();
-break;
-
-case 4:
-cout<<"Exiting the programe: "<<endl;
-return 0;
-break;
-	
-}
-
-	
-}
-while(choice!=4);
-
-	
-return 0;	
+    return 0;
 }
